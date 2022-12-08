acb5d2d6813894a97921b1b1b8c4dcde918fb86f
Yeicor <4929005+Yeicor@users.noreply.github.com>
2022-12-08 13:15:32 +0100

    Too much buffering

--- server/serve.sh
+++ server/serve.sh
@@ -2,9 +2,11 @@
 
 INPUT_LINUX="-video_size 1920x1080 -f x11grab -i :0.0+0,360"
 
-CODEC_NVENC_FMP4="-f mp4 -c:v h264_nvenc -movflags frag_keyframe+empty_moov"
-CODEC_CPU_FMP4="-f mp4 -c:v h264 -preset ultrafast -tune zerolatency -movflags frag_keyframe+empty_moov"
+CODEC_NVENC="-c:v h264_nvenc"
+CODEC_CPU="-c:v h264 -preset ultrafast -tune zerolatency"
+
+FORMAT_FMP4="-f mp4 -movflags frag_keyframe+empty_moov -frag_duration 100000" # 100ms fragments
 
 OUTPUT_HTTP_SERVER="-listen 1 http://127.0.0.1:8080/video.mp4"
 
-ffmpeg -r 30 $INPUT_LINUX -r 30 $CODEC_NVENC_FMP4 $OUTPUT_HTTP_SERVER
+ffmpeg -r 30 $INPUT_LINUX -r 30 $CODEC_NVENC $FORMAT_FMP4 $OUTPUT_HTTP_SERVER
